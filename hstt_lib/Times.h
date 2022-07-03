81573d06289720cdf17579bc1b5b8fc95a87bfbf
lancelotnd <lancelotnormand@outlook.com>
2022-07-03 14:36:16 -0400

    Added definitions of times

--- hstt_lib/Times.h
+++ hstt_lib/Times.h
@@ -6,3 +6,31 @@
  * In the datasets Weeks do not occur: all instances describe a weekly timetable. As can be guessed, a TimeGroup is a set of Times.
  * Times are assumed to be consecutive as given in the instance.
  */
+#include <vector>
+#include <map>
+
+
+
+class Time {
+    std::string name;
+    std::string id;
+    std::string ref_day;
+    std::vector<std::string> ref_timegroups;
+};
+
+
+class Times {
+    //We have an array of timegroups
+    std::map<std::string,std::string> timegroups;
+
+    //And an array of time.
+    std::vector<Time> times;
+
+    void addTime(Time t){
+        times.push_back(t);
+    }
+
+    void addTimegroup(std::string id, std::string name){
+        timegroups[id] = name;
+    }
+};
